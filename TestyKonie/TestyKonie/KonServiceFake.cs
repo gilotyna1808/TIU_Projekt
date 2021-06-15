9af92ba5c8485703cbf41b7a1a59ed287fa30017
Mateusz Gibas <mateuszgibas@gazeta.pl>
2021-06-15 23:24:22 +0200

    Dodanie testów Konie

---
+++ TestyKonie/TestyKonie/KonServiceFake.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApiKonie.Models;
+using WebApiKonie.Services;
+
+namespace TestyKonie
+{
+    class KonServiceFake:IKonService
+    {
+        private readonly ZakladyDB database;
+        public KonServiceFake(ZakladyDB zaklady)
+        {
+            database = zaklady;
+        }
+
+        public bool DodajKonia(KonDTO kon)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ModyfikujKonia(KonDTO kon)
+        {
+            throw new NotImplementedException();
+        }
+
+        public KonDTO PobierzKonia(int id)
+        {
+            return KonDTO.toKonDTO(database.Konie.Where(s => s.ID_Konia == id).First());
+        }
+
+        public IEnumerable<KonDTO> PobierzKonie()
+        {
+            var k = database.Konie.ToList();
+            return KonDTO.toKonDTO(k);
+        }
+    }
+}
---
+++ TestyKonie/TestyKonie/KonTest.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApiKonie.Controllers;
+using WebApiKonie.Models;
+using Xunit;
+
+namespace TestyKonie
+{
+   public class KonTest
+    {
+       
+        KonController _konController;
+        
+        public KonTest()
+        {
+            DbContextOptions<ZakladyDB> options = new DbContextOptionsBuilder<ZakladyDB>().UseInMemoryDatabase(databaseName: "baza").Options;
+            ZakladyDB zaklady = new ZakladyDB();
+            _konController = new KonController(zaklady);
+        }
+
+        [Fact]
+        public void DodanieNowegoKonia()
+        {
+            //Arrange
+            KonDTO kon = new KonDTO() { Kondycja = 95, Kraj = "Polska", Nazwa = "Piorun", Predkosc = 80, Wiek = 3 };
+
+            //Act 
+            var okResult = _konController.add(kon).Result as OkObjectResult;
+
+            //Assert
+            Assert.IsType<bool>(okResult.Value);
+            Assert.Equal(true, okResult.Value);
+        }
+
+        [Fact]
+        public void PobranieKoniaoDanymId()
+        {
+            //Arange
+            string nazwa = "Elf";
+
+            //Act
+            var okResult = _konController.getById(7).Result as OkObjectResult;
+
+            //Assert
+            Assert.IsType<KonDTO>(okResult.Value);
+            Assert.Equal(nazwa, (okResult.Value as KonDTO).Nazwa);
+
+        }
+
+        [Fact]
+        public void PobierzWszystkieKonie()
+        {
+            //Act
+            var okResult = _konController.getAll().Result as OkObjectResult;
+
+            //Assert
+            var items = Assert.IsType<List<KonDTO>>(okResult.Value);
+            Assert.Equal(15, items.Count);
+        }
+
+
+
+    }
+}
